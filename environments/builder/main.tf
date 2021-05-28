cce5053a663252fe296a0412bd2a319210c7adfd
Meissane-tintulip <meissane.chami+tintulip@thoughtworks.com>
2021-05-28 14:52:15 +0100

    #53 Make changes to create standard environment module

--- environments/builder/main.tf
+++ environments/builder/main.tf
@@ -9,11 +9,15 @@ module "state_bucket" {
   bucket_name = "cla-${local.environment}-state"
 }
 
-module "standard-environment" {
-  source = "../../module-template/standard-environment"
-  owner = "Governance"
-  allowed_principals = ["arn:aws:iam::${data.aws_caller_identity.builder.account_id}:root"]
-  account_id = data.aws_caller_identity.builder.account_id
-  environment = local.environment
-  tgw_name = "tgw-endpoint-${local.environment}"
+module "standard_environment" {
+  source            = "../../module-template/standard-environment"
+  owner             = "Governance"
+  account_id        = data.aws_caller_identity.builder.account_id
+  environment       = local.environment
+  tgw_name          = "tgw-endpoint-${local.environment}"
+  workload_vpc_name = "code-pipeline"
+
+  providers = {
+    aws = aws
+  }
 }
