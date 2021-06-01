75c1a1ff45fc2c145630ab17a7eb05b48589dbee
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-06-01 16:41:28 +0100

    #58 created a codepipeline webhook and randomly generated the secret.
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
    Co-authored-by: Meissane Chami <meissane.chami+tintulip@thoughtworks.com>

--- components/pipeline/main.tf
+++ components/pipeline/main.tf
@@ -45,3 +45,35 @@ resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "cla-pipeline-artifacts"
   acl    = "private"
 }
+resource "random_password" "webhooks_secret" {
+  length           = 24
+  special          = true
+  override_special = "_%@"
+}
+
+resource "aws_codepipeline_webhook" "application_infra" {
+  name            = "application-infra-webhook"
+  authentication  = "GITHUB_HMAC"
+  target_action   = "Source"
+  target_pipeline = aws_codepipeline.application_infra.name
+
+  authentication_configuration {
+    secret_token = aws_ssm_parameter.webhook_secret.value
+  }
+
+  filter {
+    json_path    = "$.ref"
+    match_equals = "refs/heads/{Branch}"
+  }
+}
+
+resource "aws_ssm_parameter" "webhook_secret" {
+  name        = "/repository/application-infra/webhook-secret"
+  description = "The secret for the webhook"
+  type        = "SecureString"
+  value       = random_password.webhooks_secret.result
+
+  tags = {
+    environment = "builder"
+  }
+}
