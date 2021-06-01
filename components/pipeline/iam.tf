04aecd0f72f4fda8b225183121db2a27747e04f4
Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
2021-06-01 16:52:57 +0100

    #58 Allow codepipeline source to access s3 bucket containing artifacts

--- components/pipeline/iam.tf
+++ components/pipeline/iam.tf
@@ -25,6 +25,20 @@ resource "aws_iam_role_policy" "codepipeline" {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObjectAcl",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.codepipeline_bucket.arn}",
+        "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+      ]
+    },
+    {
       "Effect": "Allow",
       "Action": [
         "codestar-connections:UseConnection"
