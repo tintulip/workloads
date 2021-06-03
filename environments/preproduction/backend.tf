fa3a797ac36203142356c2dfa182b43e94c88a80
Foo '0xf00' Meden <fmeden+tintulip@thoughtworks.com>
2021-06-03 15:09:56 +0100

    #49 #56 (prev commit: added role for pipeline to assume) configure preproduction stack to assume role in preproduction account, try run terraform plan in codebuild \n    Co-Authored-By: Jim G <jgumbley+co@thoughtworks.com>

--- environments/preproduction/backend.tf
+++ environments/preproduction/backend.tf
@@ -1,7 +1,24 @@
 terraform {
   backend "s3" {
-    bucket = "cla-preproduction-state"
-    key    = "pipeline-factory/preproduction.tfstate"
-    region = "eu-west-2"
+    bucket   = "cla-preproduction-state"
+    key      = "pipeline-factory/preproduction.tfstate"
+    region   = "eu-west-2"
+    role_arn = "arn:aws:iam::961889248176:role/infrastructure_pipeline"
+  }
+}
+
+locals {
+  tags = {
+    "tf:stack" = "workloads:preproduction"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = local.tags
+  }
+  assume_role {
+    role_arn = "arn:aws:iam::961889248176:role/infrastructure_pipeline"
   }
 }
---
+++ environments/preproduction/outputs.tf
@@ -0,0 +1,3 @@
+output "infra_pipeline_role_arn" {
+  value = aws_iam_role.infrastructure_pipeline.arn
+}
