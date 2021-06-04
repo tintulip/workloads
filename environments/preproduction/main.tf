3b7ace71a09c7ff3544bdb059681dff376f7c6c8
Jim G <jgumbley+co@thoughtworks.com>
2021-06-04 11:18:19 +0100

    #56 add security group for scenario 1 to workload VPC

--- environments/preproduction/main.tf
+++ environments/preproduction/main.tf
@@ -62,3 +62,31 @@ resource "aws_ecs_task_definition" "web_application" {
   container_definitions = jsonencode([
   ])
 }
+
+# the networking stuff needs to go here:
+# speculating wildly 
+# - we need a task definition 
+#         which requires a container
+#         we configure as vpc networking
+#                   relates to-> a subnet/security group in a VPC 
+#                   
+# we can then add the load balancer into that security group, and do other networking to expose it?
+# so... because we need to connect to a load of external VPC networking we'll set that up first
+
+resource "aws_security_group" "web_application_sg" {
+  name        = "web_application_sg"
+  description = "Allow http traffic for tin tulip scenario 1 web application"
+  vpc_id      = module.network.vpc_id
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
