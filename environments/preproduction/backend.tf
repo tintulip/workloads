38a4d28e88b2620d4232a789bba3691fae31699a
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-06-28 11:20:45 +0100

    #48 change the backend state bucket to the original one.
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>

--- environments/preproduction/backend.tf
+++ environments/preproduction/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket   = "tfstate-961889248176-preproduction"
+    bucket   = "cla-preproduction-state"
     key      = "pipeline-factory/preproduction.tfstate"
     region   = "eu-west-2"
     role_arn = "arn:aws:iam::961889248176:role/infrastructure_pipeline"
