55c1a33845b2fba72bafcd485d0eeb1ee1a9e8a5
Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
2021-06-28 22:25:07 +0100

    Use arn instead of string for reference to db secret

--- environments/preproduction/main.tf
+++ environments/preproduction/main.tf
@@ -115,7 +115,7 @@ resource "aws_ecs_task_definition" "web_application" {
       secrets = [
         {
           name      = "SPRING_DATASOURCE_PASSWORD"
-          valueFrom = "arn:aws:secretsmanager:eu-west-2:961889248176:secret:db_password"
+          valueFrom = aws_secretsmanager_secret.db_password.arn
 
         },
       ]
