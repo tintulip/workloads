a9acc3c9369654803215e3b510b0d691d4d9e50f
Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
2021-06-29 12:46:30 +0100

    #101 Use the right service for vpc endpoint

--- environments/preproduction/main.tf
+++ environments/preproduction/main.tf
@@ -181,33 +181,33 @@ resource "aws_security_group_rule" "allow_service_database" {
   source_security_group_id = aws_security_group.web_application_database_sg.id
 }
 
-data "aws_vpc_endpoint_service" "ssm" {
-  service = "ssm"
+data "aws_vpc_endpoint_service" "secretsmanager" {
+  service = "secretsmanager"
 }
 
-resource "aws_vpc_endpoint" "ssm" {
+resource "aws_vpc_endpoint" "secretsmanager" {
   vpc_id            = module.network.vpc_id
-  service_name      = data.aws_vpc_endpoint_service.ssm.service_name
+  service_name      = data.aws_vpc_endpoint_service.secretsmanager.service_name
   vpc_endpoint_type = "Interface"
 
-  security_group_ids = [aws_security_group.services_to_ssm.id]
+  security_group_ids = [aws_security_group.services_to_secretsmanager.id]
   subnet_ids         = module.network.private_subnets
 
   private_dns_enabled = true
 }
 
-resource "aws_security_group_rule" "allow_ssm_ingress" {
+resource "aws_security_group_rule" "allow_secretsmanager_ingress" {
   type                     = "ingress"
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.services_to_ssm.id
+  security_group_id        = aws_security_group.services_to_secretsmanager.id
   source_security_group_id = aws_security_group.web_application_service_sg.id
 }
 
-resource "aws_security_group" "services_to_ssm" {
-  name        = "services_to_ssm"
-  description = "Allow egress traffic to ssm"
+resource "aws_security_group" "services_to_secretsmanager" {
+  name        = "services_to_secretsmanager"
+  description = "Allow egress traffic to secretsmanager"
   vpc_id      = module.network.vpc_id
 }
 
