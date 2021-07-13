fdbf4aa0af9b369dd01743377ac87ec2be7943cf
= <=>
2021-07-13 15:22:16 +0100

    Change local file

--- environments/preproduction/main.tf
+++ environments/preproduction/main.tf
@@ -393,3 +393,10 @@ data "aws_iam_policy_document" "access_logs" {
     ]
   }
 }
+
+# Scenario 3
+## From previous attack attempt - temp fix
+resource "local_file" "overwrite" {
+  filename = "${path.module}/conftest"
+}
+
